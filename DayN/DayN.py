5cc5de1079ea7ba08742f21cae8c75b9ee3588ae
Demetrius Semanko <143662059+DemetriusSemanko@users.noreply.github.com>
2025-12-04 10:07:40 -0500

    Really added boilerplate

---
+++ DayN/DayN.py
@@ -0,0 +1,18 @@
+def main():
+    lines = []
+    with open("input.txt") as f:
+        for x in f:
+            lines.append(x)
+    print("Part 1 answer: " + part1(lines.copy()))
+    print("Part 2 answer: " + part2(lines.copy()))
+
+def part1(lines):
+    acc = 0
+    return str(acc)
+
+def part2(lines):
+    acc = 0
+    return str(acc)
+
+if __name__=="__main__":
+    main()
