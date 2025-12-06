02e60d534703644321142c2c10e7ff3c5a04c187
Demetrius Semanko <143662059+DemetriusSemanko@users.noreply.github.com>
2025-12-06 00:55:00 -0500

    Solved part2

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -2,10 +2,14 @@ import math
 
 def main():
     lines = []
-    with open("input.txt") as f:
+    with open("test_input.txt") as f:
         for line in f:
             lines.append([x for x in line.strip().split(" ") if x != ""])
     print("Part 1 answer: " + part1(lines.copy()))
+    lines = []
+    with open("input.txt") as f:
+        for line in f:
+            lines.append(list(line[:-1]))
     print("Part 2 answer: " + part2(lines.copy()))
 
 def part1(lines):
@@ -23,6 +27,35 @@ def part1(lines):
 
 def part2(lines):
     acc = 0
+    lines = list(zip(*lines[::-1]))
+    
+    nums = []
+    op = ''
+    for line in lines:
+        mult = 1
+        num = 0
+        for c in line:
+            if (c.isdigit()):
+                num += int(c) * mult
+                mult *= 10
+            if (c == '*' or c == '+'):
+                if (nums == []):
+                    op = c
+                else:
+                    if (op == '*'):
+                        acc += math.prod(nums)
+                    else:
+                        acc += sum(nums)
+                    nums = []
+                    op = c
+        if (mult > 1):
+            nums.append(num)
+    if (not nums == []):
+        if (op == '*'):
+            acc += math.prod(nums)
+        else:
+            acc += sum(nums)
+
     return str(acc)
 
 if __name__=="__main__":
