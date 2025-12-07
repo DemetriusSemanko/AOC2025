08cd53d20ba3d47d33bcfed80e537c03d37ef2c9
Demetrius Semanko <143662059+DemetriusSemanko@users.noreply.github.com>
2025-12-07 00:27:24 -0500

    Solved part1

--- Day7/Day7.py
+++ Day7/Day7.py
@@ -8,6 +8,28 @@ def main():
 
 def part1(lines):
     acc = 0
+    beam_cols = []
+    beam_cols.append(lines[0].index("S"))
+    line_length = len(lines[0])
+    for x in range(1, len(lines)):
+        # Get all splitter cols
+        splitter_cols = [i for i, val in enumerate(lines[x]) if val == "^"]
+        new_beam_cols = []
+        if (splitter_cols != []):
+            for beam in beam_cols:
+                if (beam in splitter_cols):
+                    acc += 1
+                    go_left = beam > 0
+                    go_right = beam < line_length - 1
+                    if (go_left):
+                        new_beam_cols.append(beam - 1)
+                    if (go_right):
+                        new_beam_cols.append(beam + 1)
+                else:
+                    new_beam_cols.append(beam)
+            new_beam_cols = set(new_beam_cols)
+            beam_cols = new_beam_cols.copy()
+
     return str(acc)
 
 def part2(lines):
