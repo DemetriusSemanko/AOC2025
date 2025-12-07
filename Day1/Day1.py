afc3981163e9ad78d43d23689f462d767fca0c17
Demetrius Semanko <143662059+DemetriusSemanko@users.noreply.github.com>
2025-12-06 21:22:57 -0500

    Added part1 soln for Day1

---
+++ Day1/Day1.py
@@ -0,0 +1,33 @@
+def main():
+    lines = []
+    with open("input.txt") as f:
+        for line in f:
+            lines.append(line.strip())
+    print("Part 1 answer: " + part1(lines.copy()))
+    print("Part 2 answer: " + part2(lines.copy()))
+
+def part1(lines):
+    acc = 0
+    pointer = 50
+    for instr in lines:
+        dir = instr[0]
+        amt = int(instr[1:])
+        
+        mult = 1
+        if (dir == "L"):
+            mult = -1    
+        
+        pointer += (mult * amt)
+        pointer %= 100
+
+        if (pointer == 0):
+            acc += 1
+
+    return str(acc)
+
+def part2(lines):
+    acc = 0
+    return str(acc)
+
+if __name__=="__main__":
+    main()
