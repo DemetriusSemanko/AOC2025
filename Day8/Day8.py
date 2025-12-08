2bb620399fa7c032d27baa350dd01934968ddb47
Demetrius Semanko <143662059+DemetriusSemanko@users.noreply.github.com>
2025-12-08 10:30:11 -0500

    Solved part1 yay

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -23,50 +23,89 @@ def part1(lines):
     distances.sort(key=lambda val : val[0])
     visited = []
     circuits = []
-    for d_tri in distances:
-        coord_a = d_tri[1]
-        coord_b = d_tri[2]
-        
+
+    # If a coord was visited, then it must
+    # belong to a circuit already
+
+    # We don't care about the distance, since we've
+    # sorted the list of distance-coord triples on distance,
+    # and we will iterate through them from closest
+    # to furthest distances.
+    max_conns = 10
+    if (len(lines) == 1000):
+        max_conns = 1000
+    conns = 1
+    #pre_conns = 0
+    for (_, coord_a, coord_b) in distances:
+        conns += 1
         if (coord_a not in visited):
             visited.append(coord_a)
             if (coord_b not in visited):
+                # If we reached here, that's because neither of the coords
+                # belong to a circuit, and since they are the currently
+                # closest pair of junction boxes, they must be connected
+                # together into a circuit of their own
                 visited.append(coord_b)
-
                 circuit = []
                 circuit.append(coord_a)
                 circuit.append(coord_b)
                 circuits.append(circuit.copy())
             else: # coord_b in visited
+                # If we reach here, then that means
+                # coord_a does NOT belong to a circuit
+                # coord_b DOES belong to a circuit
+
+                # Find the circuit coord_b belongs to
+                # and add coord_a to that circuit
                 for circuit in circuits:
                     if (coord_b in circuit):
                         circuit.append(coord_a)
-                        break
         else: # coord_a in visited
             if (coord_b not in visited):
-                # find the list with coord_a, add coord_b to it
+                # If we reach here, then that means
+                # coord_a DOES belong to a circuit
+                # coord_b does NOT belong to a circuit
                 visited.append(coord_b)
+                
+                # Find the circuit coord_a belongs to
+                # and add coord_b to that circuit
                 for circuit in circuits:
                     if (coord_a in circuit):
                         circuit.append(coord_b)
+            else:
+                # If both were visited, let's see if they are in the same
+                # circuit. If they are NOT in the same circuit, then let us
+                # combine them
+                done = False
+                for circuit_a in circuits:
+                    if (done):
                         break
-
-        #for d in distances:
-            #print(d)
-
+                    if (coord_a in circuit_a):
+                        # We found the circuit which has coord_a
+                        for circuit_b in circuits:
+                            if (coord_b in circuit_b):
+                                if (circuit_a != circuit_b):
+                                    circuit_a.extend(circuit_b)
+                                    circuits.remove(circuit_b)
+                                    done = True
+                                    break
+        if (conns == max_conns):
+            break
+    
     circuit_sizes = [len(x) for x in circuits]
-    circuit_sizes.sort()
-    # print(circuit_sizes)
+    circuit_sizes.sort(reverse=True)
+    # Checking my visited list, I called set() on it,
+    # to see if I have any duplicate visitations, which
+    # I do not. I have exactly 1000; the length of input
     # 1000 too low
-    acc += math.prod(circuit_sizes[-3:])
-    for d in distances:
-        print(d)
+    acc += math.prod(circuit_sizes[:3])
 
     return str(acc)
 
-def distance( x1, y1, z1, x2, y2, z2 ):  
-    d = math.sqrt(math.pow(x2 - x1, 2) +
-                  math.pow(y2 - y1, 2) +
-                  math.pow(z2 - z1, 2) * 1.0)
+def distance( x1, y1, z1, x2, y2, z2 ):
+    d = (math.pow(x2 - x1, 2) +
+        math.pow(y2 - y1, 2) +
+        math.pow(z2 - z1, 2))
     return d
 
 def part2(lines):
